b14214c76514125d99aeb7acc0e70f736c6bdee2
nitheen9 <nitheen99666@outlook.com>
2026-08-29 19:16:17 +0530

    Update blogger-compare.js

--- functions/api/blogger-compare.js
+++ functions/api/blogger-compare.js
@@ -3,18 +3,17 @@ export async function onRequestGet() {
     return new Response(
         JSON.stringify({
             success: true,
-            message:
-                "Blogger duplicate comparison API is working.",
             original:
                 "https://tollywoodboost.blogspot.com",
             imported:
                 "https://tollyboost.blogspot.com",
-            updated:
+            message:
+                "Blogger duplicate comparison API is working.",
+            time:
                 new Date().toISOString()
         }),
         {
             status: 200,
-
             headers: {
                 "Content-Type":
                     "application/json; charset=UTF-8",
